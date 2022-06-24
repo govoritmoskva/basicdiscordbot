b0cb1b13c6d4be6390c198a1bcd49138cc871126
manixonex <108123440+manixonex@users.noreply.github.com>
2022-06-24 14:42:53 +0300

    Update gitdiscordbot.py

--- gitdiscordbot.py
+++ gitdiscordbot.py
@@ -80,4 +80,4 @@ async def всеовшп(ctx):
 			Button(style=ButtonStyle.URL, label="✈️ Телеграм", url="https://t.me/it_proger_com")
 		]
 	)
-bot.run(yourtoken) #Включение бота
+bot.run(token) #Включение бота
